d7062a595f88f7bdbc3efe41b0fd61856895dc3d
zhn <944970483@qq.com>
2019-11-27 20:24:20 +0800

    next_film init

---
+++ src/main/java/com/next/zhn/film/NextFilmApplication.java
@@ -0,0 +1,13 @@
+package com.next.zhn.film;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextFilmApplication.class, args);
+	}
+
+}
